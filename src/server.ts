02809ed1ffbcf3d142b73af926a627c6d2168419
Wanderley Cabral da Silva <wcabraldasilvac@gmail.com>
2023-08-25 15:05:36 -0300

    :recycle: refactor code add try catch

--- src/server.ts
+++ src/server.ts
@@ -6,20 +6,28 @@ app.use(express.json())
 
 const port = process.env.PORT ?? 4000
 
-app.get('/books', async (request, response) => {
-  const books = await prismaClient.book.findMany()
-  return response.json(books)
+app.get('/books', async (req, res) => {
+    try {
+        const books = await prismaClient.book.findMany()
+        return res.json(books)
+    } catch (error) {
+        return res.status(500).json({ error: 'An error occurred while fetching books' })
+    }
 })
 
-app.post('/books', async (request, response) => {
-  const { description, name } = request.body
-  const book = await prismaClient.book.create({
-    data: {
-      description,
-      name,
-    },
-  })
-  return response.json(book)
+app.post('/books', async (req, res) => {
+    const { description, name } = req.body
+    try {
+        const book = await prismaClient.book.create({
+            data: {
+                description,
+                name,
+            },
+        })
+        return res.json(book)
+    } catch (error) {
+        return res.status(500).json({ error: 'An error occurred while creating the book' })
+    }
 })
 
-app.listen(port, () => console.log('Server is running on port ', port))
+app.listen(port, () => console.log('Server is running on port', port))
